d4d57cc98cc5f888040582bb02474fa846c91b61
bowlbird <bowlbirdcontact@gmail.com>
2024-12-22 23:55:00 -0600

    initialized base project structure

--- Server/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("versions", {
-  SendContent: (content) => ipcRenderer.send("save-content", content),
-});
