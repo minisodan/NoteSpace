64661cbdc5c26fdf21111d33338935bc1264ec5c
Mason Myles <59033177+minisodan@users.noreply.github.com>
2024-12-23 23:24:22 -0600

    React setup (#12)
    * #7 setting up react project structure, removed some initial code from tauri
    
    * setting up basic react project structure
    
    * #7 removing unneeded code, as well as possibibly changing project name

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,16 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+/// A simple Tauri command that returns the greeting 'Hi!'
+///
+/// Function will be invoked via the front end.
+fn say_hi() {}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    tauri::Builder
+        ::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![say_hi])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
