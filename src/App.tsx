24f0a649d739c8cfd51e823a8b297fd8599f138f
minisodan <masonmyles98@gmail.com>
2024-12-25 00:57:49 -0600

    #11

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 /**
  * Entry function from main.tsx which will be the main entry point for the differing Components
  */
-const App = () => {};
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
 export default App;
