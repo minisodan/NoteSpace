588d302164f7b1a8fe2de7412b758276709847f9
minisodan <masonmyles98@gmail.com>
2024-12-25 01:56:40 -0600

    #9 added in memeory text editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import Home from "./Components/Home";
+
 /**
  * Entry function from main.tsx which will be the main entry point for the differing Components
  */
-const App = () => {};
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
 
 export default App;
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>Hello!</div>;
+  return (
+    <div className="w-full h-screen flex p-0 m-0">
+      <textarea className="w-full h-full border-2 border-black p-2 box-border resize-none" />
+    </div>
+  );
 };
 
 export default Home;
