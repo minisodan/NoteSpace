eaf2fd8298367c2c5d9c73df0db992ef267d1a57
Mason Myles <59033177+minisodan@users.noreply.github.com>
2024-12-27 17:40:28 -0600

    #9 added in memeory text editing (#19)
    * #9 added in memeory text editing
    
    * #9 changed the name of the text area to editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import Editor from "./Components/Editor";
+
 /**
  * Entry function from main.tsx which will be the main entry point for the differing Components
  */
-const App = () => {};
+const App = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/Editor.tsx
@@ -0,0 +1,9 @@
+const Editor = () => {
+  return (
+    <div className="w-full h-screen flex p-0 m-0">
+      <textarea className="w-full h-full border-2 border-black p-2 box-border resize-none" />
+    </div>
+  );
+};
+
+export default Editor;
