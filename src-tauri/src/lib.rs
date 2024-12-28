4b295f0d3ae34b92cf16c1d862ec9bf965ef1bb0
bowlbird <bowlbirdcontact@gmail.com>
2024-12-27 20:43:00 -0600

    added custom titlebar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,38 +1,19 @@
-use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+use tauri::{WebviewUrl, WebviewWindowBuilder};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder
         ::default()
         .setup(|app| {
-            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl:: default())
-                .title("Transparent Titlebar Window")
-                .inner_size(800.0, 600.0);
+            // let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl:: default())
+            //     .title("Transparent Titlebar Window")
+            //     .inner_size(800.0, 600.0);
 
-            // only set transparancy for macos
-            #[cfg(target_os = "macos")]
-            let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+            // // only set transparancy for macos
+            // #[cfg(target_os = "macos")]
+            // let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
 
-            let window = win_builder.build().unwrap();
-
-            // set background color only when building for macOS
-            #[cfg(target_os = "macos")]
-            {
-                use cocoa::appkit::{NSColor, NSWindow};
-                use cocoa::base::{id, nil};
-
-                let ns_window = window.ns_window().unwrap() as id;
-                unsafe {
-                    bg_color = NSColor::colorWithRed_green_blue_alpha(
-                        nil,
-                        50.0 / 255.0,
-                        158.0 / 255.0,
-                        163.5 / 255.0,
-                        1.0,
-                    );
-                    ns_window.setBackgroundColor_(bg_color);
-                }
-            }
+            // let window = win_builder.build().unwrap();
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import "./App.css";
-
 import { getCurrentWindow } from '@tauri-apps/api/window';
-
-// when using `"withGlobalTauri": true`, you may use
-// const { getCurrentWindow } = window.__TAURI__.window;
 
 const appWindow = getCurrentWindow();
 
-document
-  .getElementById('titlebar-minimize')
-  ?.addEventListener('click', () => appWindow.minimize());
-document
-  .getElementById('titlebar-maximize')
-  ?.addEventListener('click', () => appWindow.toggleMaximize());
-document
-  .getElementById('titlebar-close')
-  ?.addEventListener('click', () => appWindow.close());
-
 const App = () => <div data-tauri-drag-region className="titlebar">
   <div className="titlebar-button" id="titlebar-minimize">
-    <img
-      src="https://api.iconify.design/mdi:window-minimize.svg"
-      alt="minimize"
-    />
+    <button onClick={() => appWindow.minimize()}>
+      min
+    </button>
   </div>
   <div className="titlebar-button" id="titlebar-maximize">
-    <img
-      src="https://api.iconify.design/mdi:window-maximize.svg"
-      alt="maximize"
-    />
+    <button onClick={() => appWindow.toggleMaximize()}>
+      max
+    </button>
   </div>
   <div className="titlebar-button" id="titlebar-close">
-    <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+    <button onClick={() => appWindow.close()}>
+      close
+    </button>
   </div>
 </div>;
 
