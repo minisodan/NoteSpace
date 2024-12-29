2c1932228b89b27011821a19f86ebc7cd8a0c917
bowlbird <bowlbirdcontact@gmail.com>
2024-12-28 21:31:21 -0600

    added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { getCurrentWindow } from '@tauri-apps/api/window';
+import Titlebar from './Components/Titlebar';
+import Editor from './Components/Editor';
 
-const appWindow = getCurrentWindow();
-
-const App = () => <div data-tauri-drag-region className="titlebar">
-  <div className="titlebar-button" id="titlebar-minimize">
-    <button onClick={() => appWindow.minimize()}>
-      min
-    </button>
+const App = () => (
+  <div>
+    <Titlebar/>
+    <Editor/>
   </div>
-  <div className="titlebar-button" id="titlebar-maximize">
-    <button onClick={() => appWindow.toggleMaximize()}>
-      max
-    </button>
-  </div>
-  <div className="titlebar-button" id="titlebar-close">
-    <button onClick={() => appWindow.close()}>
-      close
-    </button>
-  </div>
-</div>;
+)
 
 export default App;
