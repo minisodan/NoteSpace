f99f40dd72e9bbbf5cca2d4b937ec1b53a2bc68a
bowlbird <bowlbirdcontact@gmail.com>
2024-12-28 21:19:56 -0600

    fixed compile error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,15 +5,30 @@ pub fn run() {
     tauri::Builder
         ::default()
         .setup(|app| {
-            // let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl:: default())
-            //     .title("Transparent Titlebar Window")
-            //     .inner_size(800.0, 600.0);
+            #[cfg(target_os="macos")] {
+                use tauri::TitleBarStyle;
+                use cocoa::appkit::{ NSColor, NSWindow };
+                use cocoa::base::{ id, nil };
 
-            // // only set transparancy for macos
-            // #[cfg(target_os = "macos")]
-            // let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+                let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl:: default())
+                    .title("Transparent Titlebar Window")
+                    .inner_size(800.0, 600.0);
 
-            // let window = win_builder.build().unwrap();
+                let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+                let window = win_builder.build().unwrap();
+
+                let ns_window = window.ns_window().unwrap() as id;
+                unsafe {
+                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                        nil,
+                        50.0 / 255.0,
+                        158.0 / 255.0,
+                        163.5 / 255.0,
+                        1.0
+                    );
+                    ns_window.setBackgroundColor_(bg_color);
+                }
+            }
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
