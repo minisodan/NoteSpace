38d20c4b514dcfd8bef379d7a79110bac2533677
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-02 17:50:39 -0600

    #26 added tailwind custom fuctions to the bottombar component

--- src/Components/BottomBar.tsx
+++ src/Components/BottomBar.tsx
@@ -1,50 +1,80 @@
 import { PiFilePlusDuotone } from "react-icons/pi";
 import { PiFolderSimplePlus } from "react-icons/pi";
 import { PiGearLight } from "react-icons/pi";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
+
+interface OutterDivProps {
+  children: ReactNode;
+}
+
+interface InnerDivProps {
+  children: ReactNode;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+}
+
+interface InnerHoverDivProps {
+  children: string;
+}
+
+const OutterDiv = ({ children }: OutterDivProps) => {
+  return (
+    <div className="flex flex-row justify-around m-2 p-2 gap-4 text-white">
+      {children}
+    </div>
+  );
+};
+
+const InnerDiv = ({ children, onMouseEnter, onMouseLeave }: InnerDivProps) => {
+  return (
+    <div
+      className="relative p-1 rounded-md bg-transparent text-white hover:bg-gray-700 hover:text-white cursor-pointer transition duration-300"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </div>
+  );
+};
+
+const InnerHoverDiv = ({ children }: InnerHoverDivProps) => {
+  return (
+    <div className="absolute bottom-full mb-2 px-2 py-1 bg-gray-800 text-white text-sm rounded-md whitespace-nowrap">
+      {children}
+    </div>
+  );
+};
 
 const BottomBar = () => {
   const [hoveredButton, setHoveredButton] = useState<string | null>(null);
-
   return (
-    <div className="flex flex-row justify-around m-2 p-2 gap-4 text-white">
-      <div
-        className="relative p-1 rounded-md bg-transparent text-white hover:bg-gray-700 hover:text-white cursor-pointer transition duration-300"
+    <OutterDiv>
+      <InnerDiv
         onMouseEnter={() => setHoveredButton("file")}
         onMouseLeave={() => setHoveredButton(null)}
       >
         <PiFilePlusDuotone size={18} />
-        {hoveredButton === "file" && (
-          <div className="absolute bottom-full mb-2 px-2 py-1 bg-gray-800 text-white text-sm rounded-md whitespace-nowrap">
-            New File
-          </div>
-        )}
-      </div>
-      <div
-        className="relative p-1 rounded-md bg-transparent text-white hover:bg-gray-700 hover:text-white cursor-pointer transition duration-300"
+        {hoveredButton === "file" && <InnerHoverDiv>New File</InnerHoverDiv>}
+      </InnerDiv>
+      <InnerDiv
         onMouseEnter={() => setHoveredButton("folder")}
         onMouseLeave={() => setHoveredButton(null)}
       >
         <PiFolderSimplePlus size={18} />
         {hoveredButton === "folder" && (
-          <div className="absolute bottom-full mb-2 px-2 py-1 bg-gray-800 text-white text-sm rounded-md whitespace-nowrap">
-            New Folder
-          </div>
+          <InnerHoverDiv>New Folder</InnerHoverDiv>
         )}
-      </div>
-      <div
-        className="relative p-1 rounded-md bg-transparent text-white hover:bg-gray-700 hover:text-white cursor-pointer transition duration-300"
+      </InnerDiv>
+      <InnerDiv
         onMouseEnter={() => setHoveredButton("settings")}
         onMouseLeave={() => setHoveredButton(null)}
       >
         <PiGearLight size={18} />
         {hoveredButton === "settings" && (
-          <div className="absolute bottom-full mb-2 px-2 py-1 bg-gray-800 text-white text-sm rounded-md whitespace-nowrap">
-            Settings
-          </div>
+          <InnerHoverDiv>Settings</InnerHoverDiv>
         )}
-      </div>
-    </div>
+      </InnerDiv>
+    </OutterDiv>
   );
 };
 
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,11 +4,6 @@ const Sidebar = () => {
   return (
     <>
       <div className="h-screen w-52 bg-gray-900 text-white flex flex-col">
-        {/* 
-        Sidebar content will here:
-            custom title bar,
-            files and directories.       
-      */}
         <div className="mt-auto">
           <BottomBar />
         </div>
