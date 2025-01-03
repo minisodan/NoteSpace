2897e607c1a2ebf30c1ca044380950bf252c8b58
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-02 21:35:57 -0600

    #26 updated to have scroll wheeel as well as some styling

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -4,7 +4,7 @@ const Editor = () => {
   return (
     <div className="w-full h-screen flex p-0 m-0 bg-stone-900">
       <Sidebar />
-      <textarea className="w-full h-screen text-neutral-300 resize-none m-4 outline-none bg-stone-900" />
+      <textarea className="w-full text-neutral-300 resize-none m-4 outline-none bg-stone-900 scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-neutral-500 scrollbar-track-stone-900 overflow-y-auto pr-2 mr-2 scrollbar" />
     </div>
   );
 };
