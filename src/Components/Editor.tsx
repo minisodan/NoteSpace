da968c49ec6a080e25c56c8c3576524049d5eae0
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-04 12:54:52 -0600

    #26 added custom bottom bar with icons for file, folder, settings (#31)
    * #26 added custom bottom bar with icons for file, folder, settings
    
    * #26 added tailwind custom fuctions to the bottombar component
    
    * #26 added some animation for button clicking as well as general styling
    
    * #26 updated names of funtions in BottomBar to be more coherent to what the purpose of it is
    
    * #26 updated color of pop up to be consisitent
    
    * #26 updated to have scroll wheeel as well as some styling
    
    * testing scroll bar changes on linux
    
    * testing scroll bar changes on linux
    
    * final scroll bar
    
    * removed margin and adding padding
    
    * margin testing mac vs linux

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,10 +1,19 @@
 import Sidebar from "./Sidebar";
 
 const Editor = () => {
+  // Detect the platform using navigator.userAgent
+  const isMac = /Mac|iPhone|iPad/.test(navigator.userAgent);
+
   return (
-    <div className="w-full h-screen flex p-0 m-0">
+    <div className="w-full h-screen flex p-0 m-0 bg-stone-900">
       <Sidebar />
-      <textarea className="w-full h-full resize-none" />
+      <textarea
+        className={`w-full text-neutral-300 resize-none outline-none bg-stone-900 
+        scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full 
+        scrollbar-thumb-neutral-500 scrollbar-track-stone-900 hover:scrollbar-thumb-neutral-400 ${
+          isMac ? "p-2" : "p-4"
+        }`}
+      />
     </div>
   );
 };
