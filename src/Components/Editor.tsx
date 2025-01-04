27fc616089f99012a240b4613f2659eacf8c44e2
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-04 11:57:50 -0600

    testing scroll bar changes on linux

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -4,7 +4,7 @@ const Editor = () => {
   return (
     <div className="w-full h-screen flex p-0 m-0 bg-stone-900">
       <Sidebar />
-      <textarea className="w-full text-neutral-300 resize-none m-4 outline-none bg-stone-900 scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-neutral-500 overflow-y-auto pr-2 mr-2 scrollbar" />
+      <textarea className="w-full outline-none focus:outline-none overflow-hidden overflow-y-auto text-neutral-300 resize-none bg-stone-900 scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-stone-600 p-4 rounded border border-gray-700" />
     </div>
   );
 };
