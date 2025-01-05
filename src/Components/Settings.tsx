6f29718b3325caeb3cec280c793c0b2ae814b5ba
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-05 01:21:27 -0600

    #24 adding settings page

---
+++ src/Components/Settings.tsx
@@ -0,0 +1,11 @@
+const Settings = () => {
+  return (
+    <>
+      <div>Settings page</div>
+      <div>
+        <a href="/">return home</a>
+      </div>
+    </>
+  );
+};
+export default Settings;
