0196fac453eb11f3147a2bcab4bfc9474485ccf5
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-01-08 22:47:36 -0600

    added function to create new directory at users home directory (#41)

--- src/Components/Utils/FileManagement.tsx
+++ src/Components/Utils/FileManagement.tsx
@@ -1,4 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
+import { homeDir } from "@tauri-apps/api/path";
+import { join } from "@tauri-apps/api/path";
 
 export const SaveFile = ({
   content,
@@ -10,3 +12,11 @@ export const SaveFile = ({
 
 export const CreateFile = ({ path }: { path: string }) =>
   invoke("create_file", { path: path });
+
+export const CreateDirectory = async ({ name }: { name: string }) => {
+  // grabs the users base directory, this will need to change at somepoint.
+  const baseDir = await homeDir();
+  const fullPath = await join(baseDir, name);
+
+  return invoke("create_directory", { path: fullPath });
+};
