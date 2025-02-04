ee22d71854e5e3fed66df3d78ca96d41f206a5bb
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-02-03 18:19:30 -0600

    adding some content to settings page

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,11 +1,20 @@
 const Settings = () => {
   return (
-    <>
-      <div>Settings page</div>
-      <div>
-        <a href="/">return home</a>
+    <div className="flex flex-col items-center w-full h-screen p-10 m-0 bg-stone-900">
+      <div className="relative w-full text-neutral-300">
+        <div className="text-xl font-semibold uppercase text-center">
+          <h1>Settings</h1>
+        </div>
+        <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="text-xl font-semibold text-center mt-8">
+          <h1>Plug-ins</h1>
+        </div>
+        <div>
+          <a href="/">return home</a>{" "}
+          {/* This will need to change, maybe a back button in the left corner??? */}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Settings;
