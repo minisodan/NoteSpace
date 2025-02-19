2d60fbb0db128611a501b65db604079fe4bb0249
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-02-18 22:26:42 -0600

    #52 decoupling sidebar into smaller components

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,19 +1,14 @@
 import BottomBar from "../BottomBar/BottomBar";
-import Directories from "../Directories/Directories";
-import { FILE_CREATION_MODE, FileCreation, FileCreationMode } from "../FileCreation/FileCreation";
-import { DeleteFileByFullPath, FetchAllFilesAndDirectories, StripFileNameFromPath } from "../Utils/FileManagement";
+import { FILE_CREATION_MODE, FileCreationMode } from "../FileCreation/FileCreation";
+import { DeleteFileByFullPath, FetchAllFilesAndDirectories } from "../Utils/FileManagement";
 import { useState, useEffect } from "react";
-import { ConfirmationPopup } from "../Widgets/ConfirmationPopup";
+import FileManager from "./FileManager/FileManager";
 
 const Sidebar = ({ navigate }: { navigate: (path: string) => void }) => {
   const [directories, setDirectories] = useState<any[]>([]);
-
-  const [fileCreationMode, setFileCreationMode] = useState<
-    FileCreationMode | undefined
-  >(undefined);
+  const [fileCreationMode, setFileCreationMode] = useState<FileCreationMode | undefined>(undefined);
   const [fileCreationKey, setFileCreationKey] = useState<number>(0);
-
-  const [deletePath, setDeletePath] = useState<string | undefined>(undefined) 
+  const [deletePath, setDeletePath] = useState<string | undefined>(undefined)
 
   const updateFileCreation = (mode: FileCreationMode) => {
     setFileCreationKey(fileCreationKey + 1);
@@ -30,33 +25,22 @@ const Sidebar = ({ navigate }: { navigate: (path: string) => void }) => {
   });
 
   // encapsulate file creation and confirmation pop up and directories into parent componnent.
-  // re arrage file structure
   return (
     <>
       <div className="h-screen w-52 bg-neutral-800 text-white flex flex-col hide-scrollbar">
-        <div className="m-2">
-          {fileCreationMode && (
-            <FileCreation
-              key={fileCreationKey}
-              fileCreationMode={fileCreationMode ?? FILE_CREATION_MODE.FILE}
-              onComplete={fetchedData}
-            />
-          )}
-        </div>
-        {deletePath && 
-          <ConfirmationPopup 
-            bodyText={`Are you sure you want to delete '${StripFileNameFromPath({path: deletePath ?? ""})}'?`}
-            onCancel={() => setDeletePath(undefined)}
-            onConfirm={() => {
-              DeleteFileByFullPath({path: deletePath})
-              fetchedData()
-              setDeletePath(undefined)
-            }}>
-          </ConfirmationPopup>
-        }
-        <div className="flex-grow overflow-y-auto">
-          <Directories directories={directories} onDelete={(path) => {setDeletePath(path)}}/>
-        </div>
+        <FileManager
+          deletePath={deletePath}
+          directories={directories}
+          fileCreationMode={fileCreationMode}
+          fileCreationKey={fileCreationKey}
+          fetchedData={fetchedData}
+          onDelete={path => setDeletePath(path)}
+          onConfirm={() => {
+            deletePath && DeleteFileByFullPath({ path: deletePath });
+            fetchedData();
+            setDeletePath(undefined);
+          }}
+        />
         <div className="mt-auto">
           <BottomBar
             navigate={navigate}
