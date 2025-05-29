d9541a3be37857b94f06ceff9881cacc2a86e40d
bowlbird <millercarson111@gmail.com>
2025-05-28 20:28:54 -0500

    onclick sets editor contents

--- src/Components/Editor/Editor.tsx
+++ src/Components/Editor/Editor.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import { platform } from "@tauri-apps/plugin-os";
+import { useStore } from "../Utils/Store";
+import { readTextFile } from "../Utils/FileManagement";
+
 
 const Editor = () => {
+  const [content, setContent] = useState('')
+  const openFiles = useStore(state => state.openFiles)
+  
+  useEffect(() => {
+    async function fetchFileContent() {
+      const openFile = Object.keys(openFiles)[0]
+      setContent(await readTextFile({path: openFile}))
+    }
+    fetchFileContent()
+  }, [openFiles])
+
   return (
     <div className="w-full h-screen flex p-0 m-0 bg-stone-900">
       <Sidebar/>
@@ -11,6 +26,8 @@ const Editor = () => {
         scrollbar-thumb-neutral-500 scrollbar-track-stone-900 hover:scrollbar-thumb-neutral-400 mx-2 ${
           platform() === "macos" ? "p-2" : "p-4"
         }`}
+        value={content}
+        onChange={e => setContent(e.target.value)}
       />
     </div>
   );
